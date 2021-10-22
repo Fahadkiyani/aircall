b192eb27fa1bdd713c139e65d1f7e56901b8e376
Fahad kiyani <fahadkiyani08@gmail.com>
2021-10-22 21:12:59 +0500

    material ui installed

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,36 @@
 import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import Card from './components/Card.js';
-
+import axios from 'axios';
 import Header from './Header.jsx';
 
 const App = () => {
-  const [feeds, setFeeds] = useState([]);
+  const [Feeds, setFeeds] = useState([]);
+
+
+  useEffect(() => {
+    fetchFeeds();
+  }, []);
 
   const fetchFeeds = async () => {
-    let data = await fetch('https://aircall-job.herokuapp.com/activities');
-    console.log('read data: ',data);
+    let data = await axios('https://aircall-job.herokuapp.com/activities');
+    // console.log('Data: ', data.data);
+    setFeeds(data.data);
   }
 
-  useEffect(()=>{
-    fetchFeeds();
-    // console.log(feeds);
-  });
-
+  // console.log(Feeds);
+  console.log('Feeds', Feeds);
 
   return (
     <div className='container'>
       <Header heading="Activity" />
       <div style={{ backgroundColor: 'rgba(200,200,210,0.1)', height: '100%', paddingTop: 20, }}>
-        <Card name="fahad" time="12:20pm" />
+        {/* <Card name="fahad" time="12:20pm" /> */}
+        {Feeds.map(data => {
+          return(
+            <Card name={data.via} time={data.duration} />
+          )
+        })}
       </div>
     </div>
   );
